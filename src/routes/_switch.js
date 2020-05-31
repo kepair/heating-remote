18bfc1b94c64ee5ebfc8f71e5f46bed451b6d524
pedireg <40624825+pedireg@users.noreply.github.com>
2020-05-31 22:08:53 +0200

    var state

--- src/routes/_switch.js
+++ src/routes/_switch.js
@@ -1,6 +1,6 @@
 const Gpio = require('onoff').Gpio; // Gpio class
 const led = new Gpio(17, 'out'); // Export GPIO17 as an output
-state = false;
+var state = false;
 
 function switchLight() {
     // Toggle the state of the LED connected to GPIO17 every 200ms
