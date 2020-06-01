9a4d8711d6971af55a8fa3d08caa81e00b52640d
pedireg <40624825+pedireg@users.noreply.github.com>
2020-06-01 18:09:17 +0200

    ga

--- src/client.js
+++ src/client.js
@@ -1,5 +1,7 @@
 import * as sapper from '@sapper/app';
+import { googleAnalytics } from './ga.js';
 
 sapper.start({
-	target: document.querySelector('#sapper')
+    target: document.querySelector('#sapper')
 });
+googleAnalytics('UA-167982656-1');
