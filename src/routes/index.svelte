4c2c4c700fc5071bb44d8fb6c4b11b309b766ee9
pedireg <40624825+pedireg@users.noreply.github.com>
2020-06-01 10:33:26 +0200

    public urls

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,7 +9,7 @@ Live Space-Camera 2000
 <p>Is the year 2000, we achieved instant transmission of infinite information thanks to the rare Elerium-115 found on the dark side of the Moon.</p>
 <img src="/cmlogo.gif" alt="earth link">
 <p><img src="/6kb.gif" alt="earth spinning"> A window to the future:<img  src="/6kb.gif" alt="earth spinning"></p>
-<iframe title="space camera 2000" width="960px" height="540px" src="http://192.168.1.61">PONG</iframe>
+<iframe title="space camera 2000" width="960px" height="540px" src="https://c4hs7a56.kepair.eu">PONG</iframe>
 <br>
 <br>
 <button on:click={switchLight}>Switch light</button>
@@ -27,7 +27,7 @@ button{
 <script>
 	async function switchLight() {
 		console.log("calling switch")
-		const res = await fetch(`http://192.168.1.61:3000/switch.json`);
+		const res = await fetch(`https://cam.kepair.eu/switch.json`);
 		const data = await res.json();
 
 		if (res.status === 200) {
